8a799929eb30bf2fa1e873a7e61b7b498aa60e03
amarnathvattikuti <amarnathmax@gmail.com>
2022-05-06 00:11:11 +0530

    deploy changes

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
      //baseURL: "http://localhost:3006/",
-    baseURL: "https://app.netlify.com/sites/react-contact-managernew:"
+    baseURL: "https://react-new-contactmanager.netlify.app/"
     
 });
