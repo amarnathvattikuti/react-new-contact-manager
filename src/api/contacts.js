d77797ff5d667e4bd3566cdead6d929433d2fa75
amarnathvattikuti <amarnathmax@gmail.com>
2022-05-26 14:08:48 +0530

    db changes

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
      //baseURL: "http://localhost:3006/",
-    //baseURL: "https://react-new-contactmanager.netlify.app/",
-    baseURL: "https://github.com/amarnathvattikuti/react-new-contact-manager/blob/main/src/api/"
+      baseURL: "https://react-new-contactmanager.netlify.app/",
+    //baseURL: "https://github.com/amarnathvattikuti/react-new-contact-manager/blob/main/src/api/"
     
 });
